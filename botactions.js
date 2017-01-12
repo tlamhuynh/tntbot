63a9629ba50e55496f7d46568178cb11c04f8445
Lam Huynh <tlamhuynh@gmail.com>
2017-01-12 21:25:10 +0700

    update postback

--- botactions.js
+++ botactions.js
@@ -18,29 +18,25 @@ function BotActions() {
 BotActions.prototype.sendCategoriesList = function(sender){
   wooAPI.getCategories().then(function (categories){
     console.log(sender)
-    let element = botly.createListElement({
-        title: "Classic T-Shirt Collection",
-        image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-        subtitle: "See all our colors",
-        buttons: [
-            {title: "DO WORK", payload: "DO_WORK"},
-        ],
-        default_action: {
-            "url": "https://peterssendreceiveapp.ngrok.io/shop_collection",
-        }
-    });
-    let element2 = botly.createListElement({
-        title: "Number 2",
-        image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-        subtitle: "See all our colors",
-        buttons: [
-            {title: "Go to Askrround", url: "http://askrround.com"},
-        ],
-        default_action: {
-            "url": "https://peterssendreceiveapp.ngrok.io/shop_collection",
-        }
-    });
-    botly.sendList({id: sender, elements: [element, element2], buttons: botly.createPostbackButton("Continue", "continue"), top_element_style: Botly.CONST.TOP_ELEMENT_STYLE.LARGE},function (err, data) {
+    let elements = []
+    categories.map(function(category){
+        let element = botly.createListElement({
+            title: category.name,
+            image_url: category.image.src,
+            subtitle: category.description,
+            buttons: [
+                {title: "Xem sản phẩm", payload: "DO_WORK"},
+            ],
+            default_action: {
+                "url": "https://peterssendreceiveapp.ngrok.io/shop_collection",
+            }
+        });
+
+       elements.push(element)
+    })
+
+
+    botly.sendList({id: sender, elements: elements, buttons: botly.createPostbackButton("Continue", "continue"), top_element_style: Botly.CONST.TOP_ELEMENT_STYLE.LARGE},function (err, data) {
         console.log("send list cb:", err, data);
     });
   })
