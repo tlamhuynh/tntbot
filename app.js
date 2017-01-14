2a7681dabe504edc80619dd6d2016154bc191327
Lam Huynh <tlamhuynh@gmail.com>
2017-01-14 15:27:04 +0700

    update witai

--- app.js
+++ app.js
@@ -74,35 +74,21 @@ const actions = {
 // Setting up our bot
 const wit = new Wit({
   accessToken: Config.WIT_TOKEN,
-  actions :  {
-    send({sessionId}, {text}) {
-      // Our bot has something to say!
-      // Let's retrieve the Facebook user whose session belongs to
-      const recipientId = sessions[sessionId].fbid;
-      if (recipientId) {
-        // Yay, we found our recipient!
-        // Let's forward our bot response to her.
-        // We return a promise to let our bot know when we're done sending
-        return fbMessage(recipientId, text)
-        .then(() => null)
-        .catch((err) => {
-          console.error(
-            'Oops! An error occurred while forwarding the response to',
-            recipientId,
-            ':',
-            err.stack || err
-          );
-        });
-      } else {
-        console.error('Oops! Couldn\'t find user for session:', sessionId);
-        // Giving the wheel back to our bot
-        return Promise.resolve()
-      }
+  actions: {
+    send(request, response) {
+      return new Promise(function(resolve, reject) {
+        console.log(JSON.stringify(response));
+        return resolve();
+      });
     },
-    // You should implement your custom actions here
-    // See https://wit.ai/docs/quickstart
+    myAction({sessionId, context, text, entities}) {
+      console.log(`Session ${sessionId} received ${text}`);
+      console.log(`The current context is ${JSON.stringify(context)}`);
+      console.log(`Wit extracted ${JSON.stringify(entities)}`);
+      return Promise.resolve(context);
+    }
   },
-  //logger: new log.Logger(log.INFO)
+  logger: new log.Logger(log.DEBUG) // optional
 });
 
 
