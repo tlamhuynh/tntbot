f884035a72c025db3957350ee397e6cc2969ce14
Lam Huynh <tlamhuynh@gmail.com>
2017-01-17 13:30:59 +0700

    update coffee actions

--- app.js
+++ app.js
@@ -42,15 +42,8 @@ const findOrCreateSession = (fbid) => {
 
 
 const firstEntityValue = (entities, entity) => {
-  const val = entities && entities[entity] &&
-    Array.isArray(entities[entity]) &&
-    entities[entity].length > 0 &&
-    entities[entity][0].value
-  ;
-  if (!val) {
-    return null;
-  }
-  return typeof val === 'object' ? val.value : val;
+  console.log(entities)
+  return 'culi'
 };
 
 
